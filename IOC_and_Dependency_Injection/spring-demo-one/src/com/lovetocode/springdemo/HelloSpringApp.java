b1a978bafc5402f512a7b205d975cfd706bc880d
Animesh Paul <ani.nitmz@gmail.com>
2020-12-14 21:26:56 -0700

    Added Dependency Injection

--- IOC_and_Dependency_Injection/spring-demo-one/src/com/lovetocode/springdemo/HelloSpringApp.java
+++ IOC_and_Dependency_Injection/spring-demo-one/src/com/lovetocode/springdemo/HelloSpringApp.java
@@ -14,6 +14,9 @@ public class HelloSpringApp {
 		//call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(cricketCoach.getDailyWorkout());
+		
+		// calling new method for fortunes
+		System.out.println(theCoach.getDailyFortune());
 		//close the context
 		context.close();
 	}
