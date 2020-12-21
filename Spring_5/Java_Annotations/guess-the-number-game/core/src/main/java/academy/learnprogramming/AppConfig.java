3ccd7e699d611918741f044b97efb865d2cb90ca
Animesh Paul <ani.nitmz@gmail.com>
2020-12-20 20:36:03 -0700

    Added Java Annotation Configuration

---
+++ Spring_5/Java_Annotations/guess-the-number-game/core/src/main/java/academy/learnprogramming/AppConfig.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  // No need to include @Component annotation because @Configuration includes @Component
+@ComponentScan(basePackages = "academy.learnprogramming")
+public class AppConfig {
+
+    // == bean methods ==
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
+}
