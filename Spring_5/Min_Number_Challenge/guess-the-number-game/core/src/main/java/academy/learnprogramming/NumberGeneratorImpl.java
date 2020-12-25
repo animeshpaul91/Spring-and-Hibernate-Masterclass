229aa28662a04f771fe38f83b299d6312b548572
Animesh Paul <ani.nitmz@gmail.com>
2020-12-24 21:17:22 -0700

    Completed MinNumber Challenge

---
+++ Spring_5/Min_Number_Challenge/guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,35 @@
+package academy.learnprogramming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Random;
+
+public class NumberGeneratorImpl implements NumberGenerator {
+
+    // == fields ==
+    private final Random random = new Random();
+
+    @Autowired
+    @MaxNumber
+    private int maxNumber;
+
+    @Autowired
+    @MinNumber
+    private int minNumber;
+
+    // == public methods ==
+    @Override
+    public int next() {
+        return minNumber + random.nextInt(maxNumber - minNumber);
+    }
+
+    @Override
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    @Override
+    public int getMinNumber() {
+        return minNumber;
+    }
+}
