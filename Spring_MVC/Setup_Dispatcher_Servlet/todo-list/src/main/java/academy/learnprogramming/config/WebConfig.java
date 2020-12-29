e6c1e633006cf1d4a202d850e9fc2199e97c1725
Animesh Paul <ani.nitmz@gmail.com>
2020-12-28 20:15:24 -0700

    Added Dispatcher Servlet

--- Spring_MVC/Setup_Dispatcher_Servlet/todo-list/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ Spring_MVC/Setup_Dispatcher_Servlet/todo-list/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,8 +1,14 @@
 package academy.learnprogramming.config;
 
-import org.springframework.stereotype.Component;
+// This Configuraion Class Registers a Dispatcher Servlet in this Application
 
-@Component
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
 public class WebConfig {
 
 }
