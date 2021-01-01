72bf6fcda6b0dd07e6ec83fe45c88f94ed46409f
Animesh Paul <ani.nitmz@gmail.com>
2020-12-31 17:14:25 -0700

    Added TodoItem Controller

---
+++ Spring_MVC/Project_Todo_list/todo-list/src/main/java/academy/learnprogramming/util/Mappings.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class Mappings {
+
+    // == constants ==
+    public static final String ITEMS = "items";
+    // == constructors ==
+    // private enforcing this class to not be instantiated
+    private Mappings() {}
+
+}
