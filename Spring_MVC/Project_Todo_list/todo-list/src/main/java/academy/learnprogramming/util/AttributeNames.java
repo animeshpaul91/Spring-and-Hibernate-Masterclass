5341441a1fb2cfbbd855fb5309c3f7aac8eec5c7
Animesh Paul <ani.nitmz@gmail.com>
2020-12-31 22:15:02 -0700

    Implemented Post Redirect Get Pattern

---
+++ Spring_MVC/Project_Todo_list/todo-list/src/main/java/academy/learnprogramming/util/AttributeNames.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.util;
+
+public final class AttributeNames {
+    private AttributeNames() {}
+
+    // == consttants ==
+    public static final String TODO_ITEM = "todoItem";
+}
