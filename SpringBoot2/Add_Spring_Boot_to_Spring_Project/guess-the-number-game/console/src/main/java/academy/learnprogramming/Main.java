68b6c5904006a459f66e7177f4b5e98ab4f5fce7
Animesh Paul <ani.nitmz@gmail.com>
2021-01-17 18:35:32 -0700

    Converted Spring Application to SpringBoot Application

---
+++ SpringBoot2/Add_Spring_Boot_to_Spring_Project/guess-the-number-game/console/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
