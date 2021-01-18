8e9aae8adda6c7d809d576b6f7188bb7495649b4
Animesh Paul <ani.nitmz@gmail.com>
2021-01-17 20:35:58 -0700

    Added first thymeleaf view

---
+++ SpringBoot2/Thymeleaf_Setup/guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+
+    // == constructors ==
+    private ViewNames() {
+    }
+
+    // == constants ==
+    public static final String HOME = "home";
+}
