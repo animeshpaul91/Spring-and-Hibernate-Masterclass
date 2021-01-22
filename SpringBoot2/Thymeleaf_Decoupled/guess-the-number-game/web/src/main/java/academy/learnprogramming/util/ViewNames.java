f217d1c3a44786aece8cca75600b85ce51061098
Animesh Paul <ani.nitmz@gmail.com>
2021-01-21 20:40:46 -0700

    Added Thymeleaf Decoupling

---
+++ SpringBoot2/Thymeleaf_Decoupled/guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+
+    // == constructors ==
+    private ViewNames() {
+    }
+
+    // == constants ==
+    public static final String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
+}
