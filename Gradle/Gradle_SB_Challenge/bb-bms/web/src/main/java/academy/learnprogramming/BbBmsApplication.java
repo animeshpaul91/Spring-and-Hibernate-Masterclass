d95e8481193bc7cb7c9e3a74b4b178c7838a5c93
Animesh Paul <ani.nitmz@gmail.com>
2021-01-24 20:36:42 -0700

    Safe Commit

--- Gradle/Gradle_SB_Challenge/bb-bms/web/src/main/java/academy/learnprogramming/BbBmsApplication.java
+++ Gradle/Gradle_SB_Challenge/bb-bms/web/src/main/java/academy/learnprogramming/BbBmsApplication.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming.bbbms;
+package academy.learnprogramming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
