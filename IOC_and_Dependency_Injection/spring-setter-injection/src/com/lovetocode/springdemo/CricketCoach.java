1e508a35d40058336e75d62c9184386f342a594d
Animesh Paul <ani.nitmz@gmail.com>
2021-01-30 13:50:34 -0700

    Added Setter injection

--- IOC_and_Dependency_Injection/spring-setter-injection/src/com/lovetocode/springdemo/CricketCoach.java
+++ IOC_and_Dependency_Injection/spring-setter-injection/src/com/lovetocode/springdemo/CricketCoach.java
@@ -2,16 +2,30 @@ package com.lovetocode.springdemo;
 
 public class CricketCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	public CricketCoach() {
+		System.out.println("Cricket Coach: inside no args constructor");
+	}
+	
+	
+	// our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Cricket Coach: indide setter method");
+		this.fortuneService = fortuneService;
+	}
+
+
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
-		return "Daily 10 overs Batting and 15 overs bowling";
+		return "Practice fast bowling for 15 minutes";
 	}
 	
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
