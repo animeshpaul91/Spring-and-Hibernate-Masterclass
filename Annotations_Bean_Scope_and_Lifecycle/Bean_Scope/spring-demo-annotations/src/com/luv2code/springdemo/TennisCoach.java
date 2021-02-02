2bec2a9a1595516bcdc7fe018102a0ee0ba2afe4
Animesh Paul <ani.nitmz@gmail.com>
2021-02-01 19:49:38 -0700

    Added Bean Scope with Java Annotations

---
+++ Annotations_Bean_Scope_and_Lifecycle/Bean_Scope/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,30 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component // Spring will automatically register this bean with the default bean ID tennisCoach (camelcased)
+// else you can specify a custom bean ID in parenthesis after the annotation.
+@Scope("prototype")
+public class TennisCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() { 
+		return "Practice your backhand volley";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+
+}
