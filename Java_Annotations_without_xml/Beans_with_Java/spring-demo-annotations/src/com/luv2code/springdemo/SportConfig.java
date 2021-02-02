6ad98f3375c1907238da639e6ceccb5cf8c159f1
Animesh Paul <ani.nitmz@gmail.com>
2021-02-01 22:57:46 -0700

    Optimized Imports

--- Java_Annotations_without_xml/Beans_with_Java/spring-demo-annotations/src/com/luv2code/springdemo/SportConfig.java
+++ Java_Annotations_without_xml/Beans_with_Java/spring-demo-annotations/src/com/luv2code/springdemo/SportConfig.java
@@ -1,7 +1,6 @@
 package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
