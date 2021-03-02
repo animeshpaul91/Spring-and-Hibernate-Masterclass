73a96d81fda45a2229eb641fe5396b4ac116a73f
Animesh Paul <ani.nitmz@gmail.com>
2021-03-01 20:07:24 -0700

    Made Changes in Course Class

--- Hibernate/@OneToMany/Unidirectional_Mappings/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Course.java
+++ Hibernate/@OneToMany/Unidirectional_Mappings/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +30,10 @@ public class Course {
 	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
 	@JoinColumn(name="instructor_id") // @JoinColumn is a column that is always in the same table. Its the foreign key. Here its instructor_id
 	private Instructor instructor;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id") // in OneToMany Unidirectional Mapping foreign key is at the target table
+	private List<Review> reviews;
 	
 	public Course() {
 		
@@ -58,6 +67,23 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	// add a convenience method
+	public void addReview(Review review) {
+		if (reviews == null)
+			reviews = new ArrayList<>();
+		
+		reviews.add(review);
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + "]";
