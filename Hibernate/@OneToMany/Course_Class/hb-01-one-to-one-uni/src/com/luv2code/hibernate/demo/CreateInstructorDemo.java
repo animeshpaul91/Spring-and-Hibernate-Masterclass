75c80a0af2edbc259f28836e358a2bbd2f19ef00
Animesh Paul <ani.nitmz@gmail.com>
2021-03-04 19:38:28 -0700

    Minor Refactoring

--- Hibernate/@OneToMany/Course_Class/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateInstructorDemo.java
+++ Hibernate/@OneToMany/Course_Class/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateInstructorDemo.java
@@ -15,9 +15,7 @@ public class CreateInstructorDemo {
 		// create session factory
 
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
-				.addAnnotatedClass(InstructorDetail.class)
-				.addAnnotatedClass(Course.class)
-				.buildSessionFactory();
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
 
 		// create session
 		Session session = factory.getCurrentSession();
@@ -25,37 +23,37 @@ public class CreateInstructorDemo {
 		try {
 			// Create the objects
 			Instructor instructor1 = new Instructor("Susan", "Miller", "smiller@luv2code.com");
-			
+
 			InstructorDetail instructorDetail1 = new InstructorDetail("http://www.luv2code.com/gaming", "Gaming");
-			
+
 			Instructor instructor2 = new Instructor("Madhu", "Patel", "madhu@luv2code.com");
-			
+
 			InstructorDetail instructorDetail2 = new InstructorDetail("http://youtube.com/", "Guitar");
-			
+
 			// associate the objects
 			instructor1.setInstructorDetail(instructorDetail1);
 			instructor2.setInstructorDetail(instructorDetail2);
-			
+
 			// start the transaction
 			session.beginTransaction();
 
-			// save the instructor 
+			// save the instructor
 			// Note this will also save the details object because of cascadetype.ALL
 			System.out.println("Saving instructor1: " + instructor1);
 			System.out.println("Saving instructor2: " + instructor2);
-			
+
 			session.save(instructor1);
-			session.save(instructor2);			
-			
+			session.save(instructor2);
+
 			// commit transaction
 			session.getTransaction().commit();
 		}
 
-		catch(NullPointerException nullPointerException) {
+		catch (NullPointerException nullPointerException) {
 			nullPointerException.printStackTrace();
 		}
-		
-		finally {			
+
+		finally {
 			session.close();
 			factory.close();
 		}
--- Hibernate/@OneToMany/Course_Class/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/DateUtils.java
+++ Hibernate/@OneToMany/Course_Class/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/DateUtils.java
@@ -4,18 +4,16 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class DateUtils {
 
 	private static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public static Date parseDate(String dateStr) throws ParseException {
 		Date date = formatter.parse(dateStr);
 		return date;
 	}
-	
+
 	public static String formatDate(Date date) { // not being used in project
-		return date != null ? formatter.format(date): null;
+		return date != null ? formatter.format(date) : null;
 	}
-			
 }
