8bd1e85493e1c012c61f4e1e46790e452c93b601
Animesh Paul <ani.nitmz@gmail.com>
2021-03-07 17:15:24 -0700

    Added Part-2

---
+++ CRM_CRUD_Web_App/Part_2/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// need to inject the customer dao
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@GetMapping("/list")
+	public String listCustomers(Model model) {
+		
+		// get the customers from the DAO
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		model.addAttribute("customers", customers);
+		
+		return "list-customers";
+	}
+}
