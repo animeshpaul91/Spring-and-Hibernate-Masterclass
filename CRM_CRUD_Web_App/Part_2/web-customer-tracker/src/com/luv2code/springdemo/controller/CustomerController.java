e841ed393d3ec09fd362c57f3ba4f25a9f698ed6
Animesh Paul <ani.nitmz@gmail.com>
2021-03-07 17:31:34 -0700

    Added Service Layer

--- CRM_CRUD_Web_App/Part_2/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ CRM_CRUD_Web_App/Part_2/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -8,8 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
@@ -18,13 +18,13 @@ public class CustomerController {
 	// need to inject the customer dao
 	
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
 		// get the customers from the DAO
-		List<Customer> customers = customerDAO.getCustomers();
+		List<Customer> customers = customerService.getCustomers();
 		
 		// add the customers to the model
 		model.addAttribute("customers", customers);
