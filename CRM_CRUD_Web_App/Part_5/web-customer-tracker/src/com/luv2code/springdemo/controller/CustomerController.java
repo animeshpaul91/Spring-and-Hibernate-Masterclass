f7a8b10989557fd7feb3c5babda606649f50f27c
Animesh Paul <ani.nitmz@gmail.com>
2021-03-09 18:03:40 -0700

    Added Part-5

---
+++ CRM_CRUD_Web_App/Part_5/web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// need to inject the customer dao
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
+	public String listCustomers(Model model) {
+		
+		// get the customers from the DAO
+		List<Customer> customers = customerService.getCustomers();
+		
+		// add the customers to the model
+		model.addAttribute("customers", customers);
+		
+		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		// create model attribute to bind 
+		Customer customer = new Customer();
+		model.addAttribute("customer", customer);		
+		return "customer-form";
+	}
+	
+	@PostMapping("saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		return "redirect:/customer/list";
+	}
+}
---
+++ CRM_CRUD_Web_App/Part_5/web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,45 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory so that DAO can use it to talk to backend
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Customer> getCustomers() {
+
+		// get the current hibernate session
+		Session session = sessionFactory.getCurrentSession();
+
+		// create a query sorted by last name
+		Query<Customer> query = session.createQuery("from Customer order by lastName", Customer.class);
+
+		// execute query and get result list
+		List<Customer> customers = query.getResultList();
+
+		// return results
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		// get the hibernate session 
+		Session session = sessionFactory.getCurrentSession();		
+
+		// save the customer
+		session.save(customer);
+	}
+}
