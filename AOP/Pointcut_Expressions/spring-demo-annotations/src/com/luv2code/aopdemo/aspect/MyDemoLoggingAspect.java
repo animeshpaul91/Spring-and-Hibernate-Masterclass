ebcbbccd798fcb3a6bbe94b5800b4b0b63ad9f56
Animesh Paul <ani.nitmz@gmail.com>
2021-03-11 16:03:12 -0700

    Added more PointCut Expressions

---
+++ AOP/Pointcut_Expressions/spring-demo-annotations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	// this is where we add all of our related advices for logging
+	
+	// let's start with a @Before Advice
+	
+	// General Syntax 
+	// execution(modifiers-pattern? return-type-pattern declaring-type-pattern? method-name-pattern(param-pattern) throws-pattern?)
+	// ? indicates optional
+	// public - modifier
+	// void - return type
+	// com.luv2code.aopdemo.dao.AccountDAO - declaring type
+	// addAccount() - method name (here no parameters)
+	
+	@Before("execution(public void add*(com.luv2code.aopdemo.Account, ..))") // match parameters by Account then followed by 0 or more parameters
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=============>>> Executing @Before advice on addAccount()");
+	}
+}
