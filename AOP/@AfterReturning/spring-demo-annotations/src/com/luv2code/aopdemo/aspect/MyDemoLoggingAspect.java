684b257d6553712e906903f475ae53102ca5cf7d
Animesh Paul <ani.nitmz@gmail.com>
2021-03-12 20:33:44 -0700

    Post Process Completed

--- AOP/@AfterReturning/spring-demo-annotations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ AOP/@AfterReturning/spring-demo-annotations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -34,17 +34,17 @@ public class MyDemoLoggingAspect {
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint joinPoint) {
 		System.out.println("\n=============>>> Executing @Before package match advice methods()");
-		
+
 		// display the method signature
 		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
 		System.out.println("Method: " + methodSignature);
 		System.out.println();
-		
+
 		// display the method arguments
 		// get args
 		Object[] args = joinPoint.getArgs();
-		
-		for(Object arg: args) {
+
+		for (Object arg : args) {
 			System.out.println(arg);
 			if (arg instanceof Account) {
 				// downcast and print Account specific stuff
@@ -54,15 +54,35 @@ public class MyDemoLoggingAspect {
 			}
 		}
 	}
-	
+
 	// add a new advice for @AfterReturning on the findAccounts method
-	
-	@AfterReturning(pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))", returning="result")
+
+	@AfterReturning(pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result")
 	public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
-		// second argument has to match the keyword mentioned against returning in line # 60
-		
+		// second argument has to match the keyword mentioned against returning in line
+		// # 60
+
+		// print the method we are advising on
 		String method = joinPoint.getSignature().toShortString();
 		System.out.println("\n=============>>>> Executing @AfterReturning on method: " + method);
+
+		// print out the results of the method call
 		System.out.println("\n=============>>>> Result is: " + result);
+
+		// let's post-process the data. Let's modify it
+		// convert the account names to all uppercase
+		convertAccountNamesToUpperCase(result);
+		
+		System.out.println("\n=============>>>> Result is: " + result);
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		// loop through Accounts
+
+		for (Account account : result) {
+			// get uppercase version of the name
+			String upperName = account.getName().toUpperCase();
+			account.setName(upperName);
+		}
 	}
 }
