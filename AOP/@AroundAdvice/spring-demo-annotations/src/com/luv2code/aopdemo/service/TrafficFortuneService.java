4c4f6476210c89fffaf488ffe20e08091d470321
Animesh Paul <ani.nitmz@gmail.com>
2021-03-13 13:21:23 -0700

    Removed test print statement

--- AOP/@AroundAdvice/spring-demo-annotations/src/com/luv2code/aopdemo/service/TrafficFortuneService.java
+++ AOP/@AroundAdvice/spring-demo-annotations/src/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class TrafficFortuneService {
 	public String getFortune() {
-		System.out.println("...test...");
-		
 		// simulate a delay
 		try {
 			TimeUnit.SECONDS.sleep(5);
