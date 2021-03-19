f8a31491632900e8ed7201fe2e7b777b068c0b37
Animesh Paul <ani.nitmz@gmail.com>
2021-03-18 19:13:48 -0600

    Completed Spring Config for DB Connection

--- Spring_Security/spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ Spring_Security/spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,15 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,9 +19,18 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc // provides same functionality as <mvc:annotation-driven>
 @ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties") // Will read the properties file
+// src/main/resources files are automatically copied to classpath during Maven Build
 public class DemoAppConfig implements WebMvcConfigurer {
-	// define a bean for ViewResolver
 
+	// setup a variable to hold the properties
+	@Autowired
+	private Environment env;
+	
+	// set up a logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	// define a bean for ViewResolver
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -27,5 +43,25 @@ public class DemoAppConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+	}
+	
+	// define a bean for our security datasource
+	@Bean
+	public DataSource securityDataSource() {
+		// create a connection pool 
+		
+		
+		// set the jdbc driver class
+		
+		
+		// log the conenction props
+		
+		
+		// set database connection props
+		
+		
+		// set connection pool props
+		
+		return null; 
 	}
 }
