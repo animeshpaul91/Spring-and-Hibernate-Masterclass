dba9531bdc5b63a28a0ce69ee557f0651d3edbae
Animesh Paul <ani.nitmz@gmail.com>
2021-03-18 19:23:59 -0600

    Updated App Config

--- Spring_Security/spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ Spring_Security/spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,5 +1,6 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
 import java.util.logging.Logger;
 
 import javax.sql.DataSource;
@@ -16,6 +17,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc // provides same functionality as <mvc:annotation-driven>
 @ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
@@ -25,11 +28,11 @@ public class DemoAppConfig implements WebMvcConfigurer {
 
 	// setup a variable to hold the properties
 	@Autowired
-	private Environment env;
-	
+	private Environment env; // and configured via @PropertySource. Will hold the properties 
+
 	// set up a logger for diagnostics
 	private Logger logger = Logger.getLogger(getClass().getName());
-	
+
 	// define a bean for ViewResolver
 	@Bean
 	public ViewResolver viewResolver() {
@@ -44,24 +47,28 @@ public class DemoAppConfig implements WebMvcConfigurer {
 		// TODO Auto-generated method stub
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 	}
-	
+
 	// define a bean for our security datasource
 	@Bean
 	public DataSource securityDataSource() {
-		// create a connection pool 
-		
+		// create a connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
 		
 		// set the jdbc driver class
-		
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		}
 		
 		// log the conenction props
-		
-		
+		logger.info(">>>>> jdbc.url = " + env.getProperty("jdbc.url"));
+		logger.info(">>>>> jdbc.user = " + env.getProperty("jdbc.user"));
 		// set database connection props
-		
-		
+
 		// set connection pool props
-		
-		return null; 
+
+		return securityDataSource;
 	}
 }
