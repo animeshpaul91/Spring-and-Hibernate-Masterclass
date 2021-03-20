c8da9b293bd16011d61fa246ce27669e8903d87f
Animesh Paul <ani.nitmz@gmail.com>
2021-03-20 12:51:16 -0600

    Added Students get Controller

---
+++ Spring_REST/REST_Controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api") // base request controller
+public class StudentRestController {
+
+	// define an endpoint for "/students" - return a list of all students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("Poornima", "Patel"));
+		students.add(new Student("Mario", "Rossi"));
+		students.add(new Student("Mary", "Smith"));
+		return students;
+	}
+}
