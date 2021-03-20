c09a9e9181e640d30c0ea7b4f49b0ab8a63a7c5a
Animesh Paul <ani.nitmz@gmail.com>
2021-03-20 13:10:54 -0600

    Optimized REST service using @PostConstruct

--- Spring_REST/REST_Controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ Spring_REST/REST_Controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,19 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api") // base request controller
 public class StudentRestController {
 
-	// define an endpoint for "/students" - return a list of all students
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	private List<Student> students;
+	
+	// define @PostConstruct to load the student data - This is called when the bean is created
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<>();
 		students.add(new Student("Poornima", "Patel"));
 		students.add(new Student("Mario", "Rossi"));
 		students.add(new Student("Mary", "Smith"));
+	}
+	// define an endpoint for "/students" - return a list of all students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return students; // spring leverages jackson to convert list to JSON implicitly
 	}
 }
