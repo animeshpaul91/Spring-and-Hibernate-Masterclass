c1efa9fd8b1bd7a73f7705cdc24548383ac85e33
Animesh Paul <ani.nitmz@gmail.com>
2021-03-20 13:15:49 -0600

    Added Student ID endpoint using @PathVariable to access a single student

--- Spring_REST/REST_Controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ Spring_REST/REST_Controller/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,9 @@ import com.luv2code.springdemo.entity.Student;
 public class StudentRestController {
 
 	private List<Student> students;
-	
-	// define @PostConstruct to load the student data - This is called when the bean is created
+
+	// define @PostConstruct to load the student data - This is called when the bean
+	// is created
 	@PostConstruct
 	public void loadData() {
 		students = new ArrayList<>();
@@ -25,9 +27,19 @@ public class StudentRestController {
 		students.add(new Student("Mario", "Rossi"));
 		students.add(new Student("Mary", "Smith"));
 	}
+
 	// define an endpoint for "/students" - return a list of all students
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		return students; // spring leverages jackson to convert list to JSON implicitly
+	}
+
+	// define endpoint for "/students/{studentId}" - return student at index
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) { // path Variable binds to studentId mentioned in
+																// Annotation. Both have to be same
+		// just index into the list -- keep it simple for now
+		return students.get(studentId);
 	}
 }
