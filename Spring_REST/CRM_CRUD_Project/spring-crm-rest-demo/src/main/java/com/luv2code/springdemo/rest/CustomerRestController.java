8b0c389ef266f665effdc15bff1b27ea9ded664b
Animesh Paul <ani.nitmz@gmail.com>
2021-03-21 10:51:50 -0600

    Added GET for Customer by ID

--- Spring_REST/CRM_CRUD_Project/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ Spring_REST/CRM_CRUD_Project/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class CustomerRestController {
 	// add mapping for GET /customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers() {
-		return customerService.getCustomers();
+		return customerService.getCustomers(); // jackson converts List of POJOs to JSON
+	}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		Customer customer = customerService.getCustomer(customerId);
+		return customer; // jackson converts POJO to JSON
 	}
 }
