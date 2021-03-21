7ccc379c4eb2edf554cc693aae9ac9d7220a0667
Animesh Paul <ani.nitmz@gmail.com>
2021-03-21 11:13:51 -0600

    Handled Both Exceptions

--- Spring_REST/CRM_CRUD_Project/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ Spring_REST/CRM_CRUD_Project/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -11,15 +11,24 @@ public class CustomerRestExceptionHandler {
 	// Add an exception handler for CustomerNotFoundException
 	@ExceptionHandler
 	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException customerNotFoundException) {
-		
-		// create CustomerErrorResponse 
+
+		// create CustomerErrorResponse
 		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(),
-																customerNotFoundException.getMessage(), 
-																System.currentTimeMillis());
-		
+				customerNotFoundException.getMessage(), System.currentTimeMillis());
+
 		// return ResponseEntity
 		return new ResponseEntity<CustomerErrorResponse>(error, HttpStatus.NOT_FOUND);
 	}
-	
+
 	// Add another exception handler for any other type of Exception
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exception) {
+
+		// create CustomerErrorResponse
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(),
+				"Bad Input Parameter", System.currentTimeMillis());
+
+		// return ResponseEntity
+		return new ResponseEntity<CustomerErrorResponse>(error, HttpStatus.BAD_REQUEST); // ResponseEntity is just a HTTP Response Wrapper in Java
+	}
 }
