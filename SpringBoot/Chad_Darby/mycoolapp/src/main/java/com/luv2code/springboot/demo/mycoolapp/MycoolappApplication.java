fa9dd64f730d138e6681ecc0b3d3234e522f2387
Animesh Paul <ani.nitmz@gmail.com>
2021-03-22 18:36:04 -0600

    Added useful comments

--- SpringBoot/Chad_Darby/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ SpringBoot/Chad_Darby/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,13 +1,25 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; // special class that is used to helps bootstrap our springboot application
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Composed of @EnableAutoConfiguration, @ComponentScan, @Configuration 
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
+		
+		/* The above lines behind the scenes
+		 * Creates Application Context
+		 * Registers all beans
+		 * Starts the embedded server
+		 */
 	}
 
 }
+
+/* 
+@EnableAutoConfiguration - Enables Spring Boot's auto-configuration support
+@ComponentScan - Enables Component Scanning of current package. Also Recursively scans sub packages
+@Configuration - Able to register extra beans with @Bean or import other configuration classes
+*/
