394075c92e5ebe658ee62bc1754b463f855c5498
Animesh Paul <ani.nitmz@gmail.com>
2021-03-26 21:07:05 -0600

    Added POST mapping to API for adding new employee

--- SpringBoot/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ SpringBoot/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,29 +16,37 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 @RestController
 @RequestMapping("/api") // base mapping
 public class EmployeeRestController {
-	
+
 	private EmployeeService employeeService;
 
 	@Autowired
 	public EmployeeRestController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
-	
+
 	// expose "/employees" and return list of all employees
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
 		return employeeService.findAll();
 	}
-	
-	
-	// add  mapping for GET /employee/{employeeId}
+
+	// add mapping for GET /employee/{employeeId}
 	@GetMapping("/employees/{employeeId}")
 	public Employee getEmployee(@PathVariable int employeeId) {
 		Employee employee = employeeService.findById(employeeId);
-		
-		if (employee == null) throw new RuntimeException("Employee Id not found - " + employeeId);
-		
+
+		if (employee == null)
+			throw new RuntimeException("Employee Id not found - " + employeeId);
+
 		return employee;
 	}
-	
+
+	@PostMapping("/employee/new")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		// also just in case they pass an ID in the data payload
+		employee.setId(0); // force insert to DB
+		employeeService.save(employee);
+		return employee;
+	}
+
 }
