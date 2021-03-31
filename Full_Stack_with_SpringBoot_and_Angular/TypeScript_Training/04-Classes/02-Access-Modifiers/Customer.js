92386b4be3aed25c0d39a3133d90c7111309c5de
Animesh Paul <ani.nitmz@gmail.com>
2021-03-30 18:55:28 -0600

    Used Accessors

---
+++ Full_Stack_with_SpringBoot_and_Angular/TypeScript_Training/04-Classes/02-Access-Modifiers/Customer.js
@@ -0,0 +1,31 @@
+var Customer = /** @class */ (function () {
+    // by default all properties are public
+    function Customer(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this.firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+var myCustomer = new Customer("Animesh", "Paul");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
