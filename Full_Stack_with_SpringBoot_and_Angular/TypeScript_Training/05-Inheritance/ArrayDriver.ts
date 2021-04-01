3892644d3fc04c4148571687b988773e38e6f686
Animesh Paul <ani.nitmz@gmail.com>
2021-03-31 19:21:19 -0600

    Added abstract class

--- Full_Stack_with_SpringBoot_and_Angular/TypeScript_Training/05-Inheritance/ArrayDriver.ts
+++ Full_Stack_with_SpringBoot_and_Angular/TypeScript_Training/05-Inheritance/ArrayDriver.ts
@@ -2,7 +2,6 @@ import {Shape} from './Shape';
 import {Circle} from './Circle';
 import {Rectangle} from './Rectangle';
 
-let myShape = new Shape(10, 15);
 
 let myCircle = new Circle(10, 15, 20);
 
@@ -11,7 +10,6 @@ let myRectangle = new Rectangle(0, 0, 3, 7);
 // declare an array of shapes
 let shapes: Shape[] = [];
 
-shapes.push(myShape);
 shapes.push(myCircle);
 shapes.push(myRectangle);
 
