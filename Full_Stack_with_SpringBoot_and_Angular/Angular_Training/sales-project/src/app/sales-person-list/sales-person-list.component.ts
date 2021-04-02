4351ed58ff65f23e19a8e80a532d5f82bf343024
Animesh Paul <ani.nitmz@gmail.com>
2021-04-01 19:42:12 -0600

    Added Sales Person Objects

--- Full_Stack_with_SpringBoot_and_Angular/Angular_Training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Angular_Training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesPersonListComponent implements OnInit {
 
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Animesh", "Paul", "ani.nitmz@gmail.com", "50000"),
+    new SalesPerson("John", "Doe", "jdoe@gmail.com", "45000"),
+    new SalesPerson("Claire", "Murphy", "cmurphy@gmail.com", "30000"),
+    new SalesPerson("Mai", "Trung", "mtrung@gmail.com", "30000")
+  ];
   constructor() { }
 
   ngOnInit(): void {
