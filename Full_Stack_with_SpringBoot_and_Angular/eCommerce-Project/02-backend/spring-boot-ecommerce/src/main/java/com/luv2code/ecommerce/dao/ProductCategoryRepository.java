5e2ded9fa9cb0d79e2fd3ec97c02f7b2192ebfb9
Animesh Paul <ani.nitmz@gmail.com>
2021-04-02 17:27:14 -0600

    API up and running

---
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+// productCategory will be the name of the JSON entry while
+// product-category will be a reference to the path of the endpoint. Normally JPA pluralizes the entity name
+// this is the way to customize it
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
