05409b7f60af90b4f3a91a91dc260ff490b3fcba
Animesh Paul <ani.nitmz@gmail.com>
2021-04-02 15:42:03 -0600

    Added Bootstrap and Conditionals

--- Full_Stack_with_SpringBoot_and_Angular/Angular_Training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Angular_Training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -10,10 +10,10 @@ export class SalesPersonListComponent implements OnInit {
 
   // create an array of objects
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("Animesh", "Paul", "ani.nitmz@gmail.com", "50000"),
-    new SalesPerson("John", "Doe", "jdoe@gmail.com", "45000"),
+    new SalesPerson("Animesh", "Paul", "ani.nitmz@gmail.com", "70000"),
+    new SalesPerson("John", "Doe", "jdoe@gmail.com", "65000"),
     new SalesPerson("Claire", "Murphy", "cmurphy@gmail.com", "30000"),
-    new SalesPerson("Mai", "Trung", "mtrung@gmail.com", "30000")
+    new SalesPerson("Mai", "Trung", "mtrung@gmail.com", "60000")
   ];
   constructor() { }
 
