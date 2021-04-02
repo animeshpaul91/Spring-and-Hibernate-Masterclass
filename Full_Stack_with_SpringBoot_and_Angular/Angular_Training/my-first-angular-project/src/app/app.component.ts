49c402f0f044d5b4d73747bd394b784d45ceb1d4
Animesh Paul <ani.nitmz@gmail.com>
2021-04-01 19:05:17 -0600

    Added useful Comments

--- Full_Stack_with_SpringBoot_and_Angular/Angular_Training/my-first-angular-project/src/app/app.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Angular_Training/my-first-angular-project/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({ // This is called a Decorator. Similar to a Spring Annotation
+  selector: 'app-root', // this is the name of the custom tag
+  templateUrl: './app.component.html', // <app-root> is replaced by this HTML content
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'my-first-angular-project'; // title is referred to in app.component.html in double curly braces
 }
