8344eb68ed651225fde90df47ccd1e1c6d698618
Animesh Paul <ani.nitmz@gmail.com>
2021-04-03 11:21:00 -0600

    Created Product Service

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/Releasev1.0/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/Releasev1.0/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { ProductService } from './services/product.service';
   ],
   imports: [
     BrowserModule, 
-    HttpClientModule // add Httpclient module for making API requests
+    HttpClientModule // add Httpclient module for making API requests. This module becomes available to the application
   ],
   providers: [ProductService], // add Product Service to help this get injected in any part of the application
   bootstrap: [AppComponent]
--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/Releasev1.0/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/Releasev1.0/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
 
-@Injectable({
+@Injectable({ // this enables the ProductService to be injected across the application
   providedIn: 'root'
 })
+
 export class ProductService {
 
-  constructor() { }
+  private baseUrl: string = "http://localhost:8080/api/products";
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> { // this method maps type JSON data from the SpringBoot REST service to a product array
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse { // will help unwrap JSON data from REST API and make use of the _embedded entry that comes back from the API
+  _embedded: {
+    products: Product[];
+  }
 }
