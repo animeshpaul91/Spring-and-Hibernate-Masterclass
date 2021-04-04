8e74ad1d29dc0ba21ab7f80184c1d64dda5995bd
Animesh Paul <ani.nitmz@gmail.com>
2021-04-04 15:33:17 -0600

    getProductCategories() added in Product Service

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,28 +3,47 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({ // this enables the ProductService to be injected across the application
   providedIn: 'root'
 })
 
 export class ProductService {
-
+  
   private baseUrl: string = "http://localhost:8080/api/products";
+
+  private categoryUrl: string = "http://localhost:8080/api/product-category";
+
   constructor(private httpClient: HttpClient) { }
 
   getProductList(categoryId: number): Observable<Product[]> { // this method maps type JSON data from the SpringBoot REST service to a product array
     // need to build the URL based on category ID
     const searchURL: string = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchURL).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchURL).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategories)
+    );
+
+  }
+
 }
 
-interface GetResponse { // will help unwrap JSON data from REST API and make use of the _embedded entry that comes back from the API
+interface GetResponseProducts { // will help unwrap JSON data from REST API and make use of the _embedded entry that comes back from the API
   _embedded: {
     products: Product[]; // data gets binded to the Products Array defined in this project
+  }
+}
+
+interface GetResponseProductCategory { // will help unwrap JSON data from REST API and make use of the _embedded entry that comes back from the API
+  _embedded: {
+    productCategories: ProductCategory[]; // data gets binded to the Products Array defined in this project
   }
 }
