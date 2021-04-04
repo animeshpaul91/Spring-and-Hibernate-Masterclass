ed56935f8f414f2ebb2efab450de5adc22674816
Animesh Paul <ani.nitmz@gmail.com>
2021-04-04 13:17:13 -0600

    Added Required Endpoint to aPI

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200") // accept calls from web browser javascript for this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
  // Different Origin here http://localhost:4200 != http://localhost:8080
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    // behind the scenes spring will execute a query similar to select * from product where category_id=2; and
+    // expose the endpoint - http://localhost:8080/api/products/search/findByCategoryId?id=2  2 is an example here
 }
