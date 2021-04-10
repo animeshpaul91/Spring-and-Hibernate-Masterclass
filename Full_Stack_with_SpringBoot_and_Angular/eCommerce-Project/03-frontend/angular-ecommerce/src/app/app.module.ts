f72b431e7a7f4cdf2771b3ea1cab68ef2f82aa28
Animesh Paul <ani.nitmz@gmail.com>
2021-04-10 17:06:27 -0600

    Added Link to Shopping Cart

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -11,9 +11,11 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 
 const routes: Routes = [
+  {path: "cart-details", component: CartDetailsComponent}, 
   {path: 'products/:id', component: ProductDetailsComponent}, 
   {path: 'search/:keyword', component: ProductListComponent}, 
   {path: 'category/:id/:name', component: ProductListComponent}, // when path matches its going to create a new instance of ProductListComponent
@@ -32,7 +34,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes), // this functions as the root Router. These set of routes are available to the entire application
