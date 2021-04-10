6f0ef4020a30023458ed6e57eac0c03d165d60db
Animesh Paul <ani.nitmz@gmail.com>
2021-04-10 14:54:49 -0600

    Created Cart Item

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/common/product.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,10 +1,10 @@
 export class Product {
     // the property names should exactly match as is in the API returned data
-    id: number;
+    id: string;
     sku: string;
     name: string;
     description: string;
-    unitPrice: string;
+    unitPrice: number;
     imageUrl: string;
     active: boolean;
     unitsInStock: number;
