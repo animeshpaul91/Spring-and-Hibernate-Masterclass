b332325e365c913ac8c75439b782d8bd6389ad21
Animesh Paul <ani.nitmz@gmail.com>
2021-04-11 18:46:49 -0600

    Tested App

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -121,8 +121,8 @@ export class CheckoutComponent implements OnInit {
     const countryCode = formGroup.value.country.code;
     const countryName = formGroup.value.country.name;
 
-    console.log(`{formGroupName} country code: ${countryCode}`);
-    console.log(`{formGroupName} country code: ${countryName}`);
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country code: ${countryName}`);
 
     this.luv2ShopFormService.getStates(countryCode).subscribe(
       data => {
@@ -143,5 +143,8 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log(`The Email Address is: ${this.checkoutFormGroup.get('customer').value.email}`);
+    console.log(`The Shipping Address country is: ${this.checkoutFormGroup.get('shippingAddress').value.country.name}`);
+    console.log(`The Shipping Address state is: ${this.checkoutFormGroup.get('shippingAddress').value.state.name}`);
+
   }
 }
