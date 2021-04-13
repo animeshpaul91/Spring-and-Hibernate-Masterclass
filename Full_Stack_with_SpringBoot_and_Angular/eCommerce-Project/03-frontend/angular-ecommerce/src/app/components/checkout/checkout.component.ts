54915b76bdf93416bbcaece0341073675082e34d
Animesh Paul <ani.nitmz@gmail.com>
2021-04-12 19:04:13 -0600

    Started Validation

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
@@ -28,13 +28,14 @@ export class CheckoutComponent implements OnInit {
     this.checkoutFormGroup = this.formBuilder.group(
       {
         customer: this.formBuilder.group({
-          firstName: [''], // these are individual form controls
-          lastName: [''],
-          email: ['']
+          firstName: new FormControl('', [Validators.required, Validators.minLength(2)]), // these are individual form controls
+          lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+          email: new FormControl('', [Validators.required, 
+                                      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
         }),
 
         shippingAddress: this.formBuilder.group({
-          street: [''],
+          street: [''], // a form field is represented by a FormControl object
           city: [''],
           state: [''],
           country: [''],
