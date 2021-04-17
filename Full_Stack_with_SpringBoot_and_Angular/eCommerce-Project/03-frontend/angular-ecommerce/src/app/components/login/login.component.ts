10e5679482873363296ee8d5d8dce9dda1b731f3
Animesh Paul <ani.nitmz@gmail.com>
2021-04-17 17:26:09 -0600

    Added code for login component

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -10,7 +10,7 @@ import * as OktaSignIn from '@okta/okta-signin-widget'; // access to entities in
 })
 export class LoginComponent implements OnInit {
 
-  oktaSignin: OktaSignIn;
+  oktaSignin: any;
   constructor(private oktaAuthService: OktaAuthService) {
     this.oktaSignin = new OktaSignIn({
       logo: 'assets/images/logo.png',
@@ -19,15 +19,30 @@ export class LoginComponent implements OnInit {
       redirectUri: myAppConfig.oidc.redirectUri,
       authParams: {
         pkce: true, // proof key for code exchange. We will make use of dynamic secrets for passing information between our app and okta authorization server.
-        issuer: myAppConfig.oidc.issuer, 
+        issuer: myAppConfig.oidc.issuer,
         scopes: myAppConfig.oidc.scopes
       }
     }
-  );
+    );
 
   }
 
   ngOnInit(): void {
-  }
+    this.oktaSignin.remove();
 
+    // render sign in widget
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'
+    }, // render the element with the id specified in login.component.html (name should be same as the div tage in HTML)
+      (response) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuthService.signInWithRedirect(); // this will help user sign in and redirect using reidirect URI
+        }
+      },
+
+      (error) => {
+        throw error;
+      }
+    );
+  }
 }
