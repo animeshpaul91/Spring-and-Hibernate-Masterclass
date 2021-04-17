3b9596972dcf79e6171f171f405c5395ac70aa77
Animesh Paul <ani.nitmz@gmail.com>
2021-04-16 21:13:56 -0600

    Generated Stubs

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -3,8 +3,11 @@ package com.luv2code.ecommerce.service;
 import com.luv2code.ecommerce.dao.CustomerRepository;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service // Spring will pick this up during Component Scanning
 public class CheckoutServiceImpl implements CheckoutService{
@@ -17,7 +20,31 @@ public class CheckoutServiceImpl implements CheckoutService{
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // retrieve the order info from the DTO
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with order items
+
+        // populate order with billing Address and shipping address
+
+        // populate customer with order
+
+        // save to database
+
+        // return response
         return null;
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID (UUID version-4) Universal Unique Identifier
+
     }
 }
