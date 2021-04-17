8da2787e522d38bc52c2f622a8d534af2f068d4a
Animesh Paul <ani.nitmz@gmail.com>
2021-04-17 11:28:44 -0600

    Added some useful comments

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/StateRepository.java
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/StateRepository.java
@@ -12,4 +12,8 @@ import java.util.List;
 @RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer> {
     List<State> findByCountryCode(@Param("code") String code);
+    /* since State entity class contains Country entity class, behind the scenes this will perform
+       a join of State and Country and filter all the records with name passed in code (say "IN") and
+       return all columns of state that match the where clause.
+     */
 }
