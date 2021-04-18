4d7a577246407b76cc66c1df22474f0f56093435
Animesh Paul <ani.nitmz@gmail.com>
2021-04-18 11:52:27 -0600

    Minor Fixes

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
-    private CheckoutService checkoutService;
+    private final CheckoutService checkoutService;
 
     @Autowired
     public CheckoutController(CheckoutService checkoutService) {
