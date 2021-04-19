db976f03ae977b4af4aa127e45d94e47173416a5
Animesh Paul <ani.nitmz@gmail.com>
2021-04-18 19:16:06 -0600

    Added 401 Unauthorized message to return to 401 HTTP responses

--- Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
+++ Full_Stack_with_SpringBoot_and_Angular/eCommerce-Project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.luv2code.ecommerce.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -35,5 +36,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // this will help enable the backend App to read the bearer token coming across the request header
 
         http.cors();
+
+        // force a non-empty response body for 401's to make the response body more friendly
+        Okta.configureResourceServer401ResponseBody(http);
     }
 }
