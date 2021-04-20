99d8c72f9cb436b72adeecea3d73b0220fa1f14b
Animesh Paul <ani.nitmz@gmail.com>
2021-04-19 19:30:42 -0600

    Created Simple REST Controller

--- Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/backend/springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/backend/springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,20 @@ package com.in28minutes.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication 
+// indicates that this is a spring context file
+// enables auto configuration
+// enables Component Scanning recursively from within this package
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ConfigurableApplicationContext applicationContext =  SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		
+//		for(String string: applicationContext.getBeanDefinitionNames()) {
+//			System.out.println(string);
+//		}
 	}
 
 }
