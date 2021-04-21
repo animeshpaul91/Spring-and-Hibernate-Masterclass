1780bfc871636064a2f081bd783a7b055cb28996
Animesh Paul <ani.nitmz@gmail.com>
2021-04-20 22:31:18 -0600

    Added more repository methods

--- Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/backend/jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
+++ Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/backend/jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learning.jpa.jpain10steps;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +13,27 @@ import com.in28minutes.learning.jpa.jpain10steps.entity.User;
 import com.in28minutes.learning.jpa.jpain10steps.service.UserRepository;
 
 @Component
-public class UserRepositoryCommandLineRunner implements CommandLineRunner{
+public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	private static final Logger log = LoggerFactory.getLogger(UserRepositoryCommandLineRunner.class);
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		User user = new User("Jill", "Smith");
-		
+
 		// New user is created in in memory Database
 		userRepository.save(user);
-		log.info("New User is Created: " + user);
+
+		Optional<User> userWithId1 = userRepository.findById(1L);
+		log.info("New User is Retrieved: " + userWithId1);
+
+		List<User> users = userRepository.findAll();
+
+		log.info("All users: " + users);
+
 	}
 }
