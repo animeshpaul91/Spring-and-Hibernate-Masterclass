27aa205227ff69ff347a0b145c3abcae1c0ea4a7
Animesh Paul <ani.nitmz@gmail.com>
2021-04-22 22:44:20 -0600

    Added Data Binding MEthods

--- Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/login/login.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string = 'in28Minutes';
+  password = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleLogin(): void {
+    console.log(this.username);
+  }
 }
