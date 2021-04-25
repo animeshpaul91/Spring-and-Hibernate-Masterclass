184f5fa59e5ef06d66ba158ec7428723f0923e1e
Animesh Paul <ani.nitmz@gmail.com>
2021-04-25 11:14:22 -0600

    Update feature Completed

--- Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/services/data/todo-data.service.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/services/data/todo-data.service.ts
@@ -20,4 +20,11 @@ export class TodoDataService {
   retrieveTodo(username: string, id: number) {
     return this.httpClient.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
   }
+
+  updateTodo(username: string, id: number, todo: Todo) {
+    return this.httpClient.put<Todo>(
+                                `http://localhost:8080/users/${username}/todos/${id}`, 
+                                todo);
+  }
+
 }
--- Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/todo/todo.component.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TodoDataService } from '../services/data/todo-data.service';
 import { Todo } from '../todo';
 
@@ -12,21 +12,27 @@ export class TodoComponent implements OnInit {
 
   id: number = 0;
   todo: Todo;
-  
-  constructor(private todoService: TodoDataService, private activatedRoute: ActivatedRoute) { 
+
+  constructor(private todoService: TodoDataService, private activatedRoute: ActivatedRoute, private router: Router) {
     this.todo = new Todo(-1, "", false, new Date());
   }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.id = this.activatedRoute.snapshot.params['id'];
     this.todoService.retrieveTodo("in28minutes", this.id)
-    .subscribe(
+      .subscribe(
         data => this.todo = data
-    );    
+      );
   }
 
   saveTodo() {
-
+    this.todoService.updateTodo("in28minutes", this.id, this.todo)
+      .subscribe(
+          data => {
+            this.todo = data;
+            this.router.navigate(['/todos']);
+          }
+      );
   }
 
 }
