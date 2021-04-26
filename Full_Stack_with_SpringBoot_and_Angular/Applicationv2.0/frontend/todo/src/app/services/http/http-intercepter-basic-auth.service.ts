37a9e1c76fb89b6e38a859e4dcc1701e23f0ae76
Animesh Paul <ani.nitmz@gmail.com>
2021-04-25 18:55:12 -0600

    Refactored HTTP Interceptor

--- Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/services/http/http-intercepter-basic-auth.service.ts
+++ Full_Stack_with_SpringBoot_and_Angular/Applicationv2.0/frontend/todo/src/app/services/http/http-intercepter-basic-auth.service.ts
@@ -1,25 +1,28 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { BasicAuthenticationService } from '../basic-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpIntercepterBasicAuthService implements HttpInterceptor{
+export class HttpIntercepterBasicAuthService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private basicAuthenticationService: BasicAuthenticationService) { }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let username: string = "in28minutes";
-    let password: string = "dummy";
-    let basicAuth: string = "Basic " + window.btoa(username + ":" + password);
+    let basicAuth = this.basicAuthenticationService.getAuthenticatedToken();
+    let username = this.basicAuthenticationService.getAuthenticatedUser();
 
-    request = request.clone(
-      {
-        setHeaders: {
-          Authorization: basicAuth
-        }
-      }); // mutating the request with Authorization header
+    if (basicAuth && username) {
+      request = request.clone(
+        {
+          setHeaders: {
+            Authorization: basicAuth
+          }
+        }); // mutating the request with Authorization header
+    }
 
-      return next.handle(request);
+    return next.handle(request); // let next http handler execute the request
   }
 }
